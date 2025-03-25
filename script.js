ae40b307b1b37dd2f87f046a6f7b072a7b6054dc
admin <timergalievip1@gmail.com>
2025-03-25 22:48:51 +0500

    добавлен рестарт теста

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<!--Таймер-->
 let time = 1200;
 const Timer = document.getElementById('timer');
 const timer = setInterval(UpdateTimer, 1000);
@@ -14,9 +15,14 @@ function UpdateTimer(){
     }
 }
 
+<!--Основные функции теста-->
 let questionIndex = 0;
 const questions = document.querySelectorAll('.question');
 const progressNumbers = document.querySelectorAll('.number');
+let trueAnswers = 0;
+let falseAnswers = 0;
+const passExam = document.getElementById('passExam');
+const failExam = document.getElementById('failExam');
 
 questions[questionIndex].classList.add('active');
 progressNumbers[questionIndex].classList.add('active');
@@ -26,9 +32,13 @@ document.querySelectorAll('.question button').forEach(button => {
         const correct = button.getAttribute('data-correct') === 'true';
 
         if(correct){
-            alert('Правильно');
+            trueAnswers++;
         }else{
-            alert('Неправильно');
+            falseAnswers++;
+            if (falseAnswers>2){
+                failExam.classList.add('active');
+                clearInterval(timer);
+            }
         }
 
         questions[questionIndex].classList.remove('active');
@@ -44,8 +54,36 @@ document.querySelectorAll('.question button').forEach(button => {
             questions[questionIndex].classList.add('active');
             progressNumbers[questionIndex].classList.add('active');
         }else{
-            alert('Вы прошли тест');
+            passExam.classList.add('active');
             clearInterval(timer);
         }
     });
+});
+
+<!--Рестарт теста-->
+
+function restartTest(){
+    time=1200;
+
+    questions.forEach(q=>q.classList.remove('active'));
+    questions[0].classList.add('active');
+
+    progressNumbers.forEach(num=>{
+        num.classList.remove('active','completed');
+    });
+    progressNumbers[0].classList.add('active');
+
+    questionIndex=0;
+    trueAnswers=0;
+    falseAnswers=0;
+
+    passExam.classList.remove('active');
+    failExam.classList.remove('active');
+
+    clearInterval(timer);
+    timer = setInterval(UpdateTimer,1000);
+}
+document.querySelectorAll('.restart').forEach(but=>{
+    but.addEventListener('click',restartTest);
+
 });
