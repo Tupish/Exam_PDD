f30cb1e993e9751da8acb94306758d997861bc91
admin <timergalievip1@gmail.com>
2025-04-01 21:53:51 +0500

    Последняя версия

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function UpdateTimer(){
 
 <!--Функция перемешивания массива-->
 function mixArray(array){
-    for (let i = array.length - 1; i > 0; i--){
+    for(let i = array.length - 1; i > 0; i--){
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
